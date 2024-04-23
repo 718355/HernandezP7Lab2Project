571ffc79c9184c8794971fb66ffb3d51c203c626
718355 <145615429+718355@users.noreply.github.com>
2024-04-22 22:23:07 -0700

    Submitted the lab 3 project video.

--- Lab2ProjectHernandezP7/Assets/Scripts/PlayerController.cs
+++ Lab2ProjectHernandezP7/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float zBound = 10.0f;
+    private float zBound = 6.0f;
 
     public float moveSpeed = 10.0f;
     // Start is called before the first frame update
